401f38cf2a00b4ac6a5b559adcb861f5eccd3208
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-01-30 20:15:12 +0600

    Array task4 complete

---
+++ task4.js
@@ -0,0 +1,16 @@
+/**
+ * Reverse the words of a sentence. Only the position of hte word will be revered. Check out the output
+ */
+
+
+let statement = "I am a hard working person";
+
+
+let revStatement = statement.split(' ').reverse().join(' ');
+console.log(revStatement);
+
+
+
+
+
+
