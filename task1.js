643525e2c4d6bebb0b8c5ca9f8e054d308731a30
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-01-30 17:17:47 +0600

    Colplet Array task1

--- task1.js
+++ task1.js
@@ -1,3 +1,12 @@
 /**
  * Write a JavaScript code to revers the array colors without using the reverse method
  */
+
+let color = ["red", "blue", "green", "yellow", "orange"];
+let  reversColor = [];
+
+for(let i =0; i<color.length; i++){
+    reversColor.unshift(color[i]);
+}
+
+console.log(reversColor);
